61a565485d122f56a5cec1af1992c1f2d65386c7
Nandoskar <snando01@kenvue.com>
2023-07-07 20:23:39 +0530

    commit the changes

--- models/L1/VW_CRITEO_AD__DAYREPORT_PRODUCT.sql
+++ models/L1/VW_CRITEO_AD__DAYREPORT_PRODUCT.sql
@@ -4,8 +4,8 @@
 with final as (
 
 select distinct a.retailer_id, b.retailer_name, count(a.retailer_id) as total_retailer_id, min(date) as Start_date, max(date) as End_date
-from source {{('DIGITAL_SHELF','KENVUE_CRITEO_AD__DAYREPORT_PRODUCT')}} a
-left join souce {{('DIGITAL_SHELF','KENVUE_CRITEO_RETAILERS')}} b
+from  {{ source('DIGITAL_SHELF','KENVUE_CRITEO_AD__DAYREPORT_PRODUCT') }}  a
+left join  {{ source ('DIGITAL_SHELF','KENVUE_CRITEO_RETAILERS') }}  b
 on  a.retailer_id = b.retailer_id
 group by 1,2
 
--- models/RAW/TBL_KENVUE_CRITEO_AD__DAYREPORT_ITEM.sql
+++ models/RAW/TBL_KENVUE_CRITEO_AD__DAYREPORT_ITEM.sql
@@ -8,8 +8,8 @@ with final as (
 
 select a.retailer_id, b.retailer_name, count(a.retailer_id) as total_retailer_id, min(date) as Start_date
 , max(date) as End_date
-from {{ source('KENVUE','KENVUE_CRITEO_AD__DAYREPORT_LINE_ITEM') }} a
-left join {{ source('DIGITAL_SHELF','KENVUE_CRITEO_RETAILERS')}} b
+from  {{ source ('KENVUE','KENVUE_CRITEO_AD__DAYREPORT_LINE_ITEM') }}  a
+left join  {{ source ('DIGITAL_SHELF','KENVUE_CRITEO_RETAILERS') }} b
 on  a.retailer_id = b.retailer_id
 group by 1,2
 
